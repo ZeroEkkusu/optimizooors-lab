43b78763ed9835f11e61bd5edeeb7c41182819ed
0xrin1 <0xrin1@protonmail.com>
2022-03-13 01:20:16 +0000

    GFlasks is GUnit

--- src/GFlasks.sol
+++ src/GFlasks.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: Unlicense
 
-import "ds-test/test.sol";
+import "src/GUnit.sol";
 
 /// @title GFlask
 /// @author Zero Ekkusu
 /// @notice Measure gas savings with different optimizations
-contract GFlasks is DSTest {
+contract GFlasks is GUnit {
     mapping (string => uint256) private gasUnoptimized;
     mapping (string => uint256) private funCounter;
 
@@ -17,7 +17,7 @@ contract GFlasks is DSTest {
         uint256 startGas = gasleft();
         _;
         uint256 endGas = gasleft();
-        (uint256 unoptimizedGas, uint256 counter) = gFlask(
+        (uint256 unoptimizedGas, uint256 counter) = GUnit.gFlask(
             false,
             startGas - endGas,
             gasUnoptimized[group],
@@ -31,7 +31,7 @@ contract GFlasks is DSTest {
         uint256 startGas = gasleft();
         _;
         uint256 endGas = gasleft();
-        (uint256 unoptimizedGas, uint256 counter) = gFlask(
+        (uint256 unoptimizedGas, uint256 counter) = GUnit.gFlask(
             true,
             startGas - endGas,
             gasUnoptimized[group],
@@ -39,38 +39,5 @@ contract GFlasks is DSTest {
         );
         gasUnoptimized[group] = unoptimizedGas;
         funCounter[group] = counter;
-    }
-
-    function gFlask(
-        bool _optimized,
-        uint256 gas,
-        uint256 unoptimizedGas,
-        uint256 counter
-    ) private returns (uint256, uint256) {
-        if (gas == 10) {
-            return (10, counter);
-        }
-        if (!_optimized) {
-            return (gas, counter);
-        }
-        emit log("");
-        emit log_named_uint("::", ++counter);
-        int256 savings = int256(unoptimizedGas) - int256(gas);
-        bool saved = savings > 0;
-        if (savings == 0) {
-            emit log("No savings.");
-        } else {
-            emit log_named_int(
-                saved ? "SAVES (GAS)" : "[!] More expensive (gas)",
-                savings
-            );
-        }
-        if (savings > 0) {
-            uint256 per = (unoptimizedGas * 1e2) / 100;
-            per = (uint256(savings) * 1e4) / per;
-            emit log_named_decimal_uint("PERCENT (%)", per, 2);
-        }
-
-        return (unoptimizedGas, counter);
     }
 }
